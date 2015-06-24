d3f0743bfa4e6c3441ca5c156c08d2ddcb585762
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-06-24 08:43:23 +0200

    Add tests to the CRC32 module

--- src/crc32.rs
+++ src/crc32.rs
@@ -103,3 +103,22 @@ impl<R: Read> Read for Crc32Reader<R>
         Ok(count)
     }
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn samples() {
+        assert_eq!(super::update(0, b""), 0);
+
+        // test vectors from the iPXE project (input and output are bitwise negated)
+        assert_eq!(super::update(!0x12345678, b""), !0x12345678);
+        assert_eq!(super::update(!0xffffffff, b"hello world"), !0xf2b5ee7a);
+        assert_eq!(super::update(!0xffffffff, b"hello"), !0xc9ef5979);
+        assert_eq!(super::update(!0xc9ef5979, b" world"), !0xf2b5ee7a);
+
+        // Some vectors found on Rosetta code
+        assert_eq!(super::update(0, b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"), 0x190A55AD);
+        assert_eq!(super::update(0, b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"), 0xFF6CAB0B);
+        assert_eq!(super::update(0, b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"), 0x91267E8A);
+    }
+}
