33cd959b11e41f4373c149f4afee88c4d90f6718
Marli Frost <marli@frost.red>
2020-08-19 13:33:37 +0100

    docs: elaborate on purpose of crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-//! A basic ZipReader/Writer crate
+//! An ergonomic API for reading and writing ZIP files.
+//!
+//! The current implementation is based on [PKWARE's APPNOTE.TXT v6.3.9](https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT)
+// TODO: Decide on the crate's bias: Do we prioritise permissiveness/correctness/speed/ergonomics?
 
 #![warn(missing_docs)]
 
