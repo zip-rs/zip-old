aa4f8174e52f909e7efadd55f3406b5c1ebc535e
Marli Frost <marli@frost.red>
2020-11-10 17:27:28 +0000

    refactor: use diff friendly documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,106 @@
-//! ZIP [`Archive`]s store files in a cross-platform format to be saved and
-//! shared.
+//! ZIP [`Archive`]s
+//! store files in a cross-platform format
+//! to be saved and shared.
 //!
-//! The ZIP format was published by PKWARE in 1989, and its age shows. It wasn't
-//! designed for the modern internet, with no global timestamps or reliable
-//! streaming support. However, time has given it the chance to creep innto
-//! every corner of computing.
+//! The ZIP format was published by PKWARE in 1989, and its age shows.
+//! It wasn't designed for the modern internet,
+//! with no global timestamps
+//! or reliable  streaming support.
+//! However, over time it has crept into every corner of computing.
 //!
-//! It's implemented for [just][zipfile] [about][ZipFile] [every][util.zip]
-//! [environment][miniz]. It's been extended into JAR, Open Office XML, and
-//! [EPUB]. Due to consumer OS support for ZIP files, it's also perfect for
-//! file distribution: chances are you've downloaded one yourself.
+//! It's implemented for
+//! [just](https://docs.python.org/3/library/zipfile.htm)
+//! [about](https://docs.microsoft.com/en-us/dotnet/api/system.io.compression.zipfil)
+//! [every](https://docs.oracle.com/javase/8/docs/api/index.html?java/util/zip/package-summary.htm)
+//! [environment](https://github.com/richgel999/miniz).
+//! It's been extended into
+//! JAR, Open Office XML,
+//! and [EPUB](https://docs.rs/epub/).
+//! Due to consumer OS support for ZIP files,
+//! it's also perfect for  file distribution:
+//! chances are you've downloaded one yourself.
 //!
 //! # Purpose
 //!
-//! This library focuses on providing an ergonomic API for manipulating ZIP
-//! archives. It makes it easy to create archives, and is flexible enough to
-//! implement a more complex format.
+//! This library focuses on
+//! providing an ergonomic API for manipulating ZIP archives.
+//! It makes it easy to create archives,
+//! and is flexible enough to implement a more complex format.
 //!
 // 1. Ergonomics
-//! - Ease of use comes first. The library will sacrifice some performance for
-//!   the sake of an intuitive interface.
+//! - Ease of use comes first.
+//!   The library will
+//!   sacrifice some performance for the sake of an intuitive interface.
 // 2. Correctness
-//! - Archives will be accurately represented by the API. If the meaning of any
-//!   metadata is unclear, the user will need to resolve it.
+//! - Archives will be accurately represented by the API.
+//!   If the meaning of any metadata is unclear,
+//!   the user will need to resolve it.
 // 3. Speed
-//! - Parsing should be as competitive with other popular implementations. [See
-//!   the benchmarks](#)
+//! - Parsing should be competitive with other popular implementations.
+//!   [See the benchmarks](#)
 // 4. Permissiveness
-//! - Invalid archives may be read if validation is not required, allowing more
-//!   ZIP-like files to be used.
+//! - Invalid archives may be read if validation is not required,
+//!   allowing more ZIP-like files to be used.
 //!
 //! # The Format
 //!
 //! ## Compression
 //!
-//! File compression is a way of reducing the size of your files by removing
-//! redundant data. ZIP uses a set of "lossless" compression algorithms to
-//! make archives as small as possible
+//! File compression is a way of reducing the size of your files
+//! by removing redundant data.
+//! ZIP uses a set of "lossless" compression algorithms
+//! to make archives as small as possible.
 //!
-//! > Note: Compression algorithms are able to be configured and optimised for
-//!         your files to use even less space
+//! > Note: Compression algorithms are able to
+//!         be configured and optimised for your files
+//!         to use even less space
 //!
-//! ZIP supports many compression formats, and this library is not compatible
-//! with all of them yet. Luckily, most ZIP files share the same few formats.
+//! ZIP supports many compression formats,
+//! and this library is not compatible with all of them yet.
+//! Luckily, most ZIP files share the same few formats.
 //!
 // TODO: List pros and cons of available formats
 //!
 //! ## Encryption
 //!
-//! It is also possible to encrypt the contents of a ZIP file. Using this
-//! feature, you can use a passphrase to keep private information safe.
+//! It is also possible to encrypt the contents of a ZIP file.
+//! Using this feature,
+//! you can use a passphrase
+//! to keep private information safe.
 //!
 // TODO: Add link to explanation
-//! > **Be careful** when using this feature, especially to provide privacy to
-//!   your users - ZIP encryption only protects a portion of the archive.
+//! > **Be careful** when using this feature,
+//!   especially to provide privacy to your users
+//!     - ZIP encryption only protects a portion of the archive.
 //!
 // FIXME: Move these details to the implementation?
 //! ### Traditional PKWARE Encryption
 //!
-//! Sometimes known as "ZipCrypto", this has been [found to be vulnerable][Known
-//! Plaintext Attack]. Unfortunately, it is the most widely supported spec
-//! anyway, and you'll find many archives using it.
+//! Sometimes known as "ZipCrypto",
+//! this has been [found to be vulnerable][Known Plaintext Attack].
+//! Unfortunately, it is the most widely supported spec anyway,
+//! and you'll find many archives using it.
 // TODO: Explain this library's encryption features - we support decryption!
 //!
 //! ### AE-x
 //!
-//! Taking advantage of the well-known AES (that's the *Advanced* Encryption
-//! Standard to you) cipher, this method can reliably secure files so long as
-//! your recipient can read it. For now it has very spotty support, and is
-//! notably missing from Windows Explorer.
+//! Taking advantage of the well-known AES
+//! (that's the Advanced Encryption Standard)
+//! cipher,
+//! this method can reliably secure files
+//! so long as your recipient can read it.
+//! For now it has very spotty support,
+//! and is notably missing from Windows Explorer.
 //!
 //! ## Extensions
 //!
-//! Files is a ZIP archive each have a header containing a list of extensible
-//! fields which contain third party metadata. They don't have a defined format,
-//! meaning this library cannot parse them and leaves the task to the user.
+//! Files in a ZIP archive
+//! each have a header
+//! containing a list of extensible fields
+//! which contain third party metadata.
+//! They don't have a defined format,
+//! meaning this library cannot parse them
+//! and leaves the task to the user.
 //!
 // TODO: Provide an example of implementing an extension
 //!
@@ -89,13 +115,6 @@
 //! ```
 //!
 //! [Known Plaintext Attack]: https://link.springer.com/content/pdf/10.1007%2F3-540-60590-8_12.pdf
-//!
-//! [zipfile]: https://docs.python.org/3/library/zipfile.html
-//! [util.zip]: https://docs.oracle.com/javase/8/docs/api/index.html?java/util/zip/package-summary.html
-//! [ZipFile]: https://docs.microsoft.com/en-us/dotnet/api/system.io.compression.zipfile
-//! [miniz]: https://github.com/richgel999/miniz
-//!
-//! [EPUB]: https://docs.rs/epub/
 use std::io;
 
 pub mod metadata;
@@ -148,12 +167,16 @@ impl<D, M> Archive<D, M> {
     /// # Warnings
     ///
     // TODO: Improve explanation
-    /// The [`io`] traits are implemented for shared references to std types,
-    /// which means you may read from the [`File`]s yielded by this iterator.
+    /// The [`io`] traits
+    /// are implemented for shared references to std types,
+    /// which means you may read from
+    /// the [`File`]s yielded by this iterator.
     ///
-    /// Be careful with these references though - each one refers to the same
-    /// object, and share its position. You can only read from the latest
-    /// [`File`] yielded.
+    /// Be careful with these references though
+    /// - each one refers to the same object,
+    /// and share its position.
+    /// You can only read from
+    /// the most recently yielded [`File`].
     ///
     /// ```
     /// // TODO: Example
@@ -195,7 +218,8 @@ impl<'a, D, M> IntoIterator for &'a Archive<D, M> {
     }
 }
 
-/// An iterator over files within an [`Archive`]
+/// An iterator over
+/// files within an [`Archive`]
 ///
 /// # Warnings
 ///
