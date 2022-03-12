6273353b523abec8881303bdccebcaff6d078855
Marli Frost <marli@frost.red>
2022-03-12 09:01:44 +0000

    docs: simplify example code

--- examples/extract_to_stdout.rs
+++ examples/extract_to_stdout.rs
@@ -13,30 +13,25 @@ use std::io;
 pub fn main() -> io::Result<()> {
     let path = std::env::args().nth(1).expect("Usage: zip-extract <path>");
 
-    // open up the zip archive
+    // open up the zip archive and prepare an extra file handle to read the file directory
     let mut footer = zip::Footer::read_from_io(std::fs::File::open(&path)?)?;
-
-    // and prepare an extra file handle to read file contents
     let mut reader = footer
         .as_mut()
         .cloned()
         .with_disk(std::fs::File::open(&path)?);
 
-    // also, allocate the structures we will need to use for decompression of all enabled methods
+    // also, allocate the structures we will use for decompression
     let mut decompressor = zip::file::Decompressor::default();
 
-    // look for the directory in this disk, and move to it
+    // look for the directory in this disk, and start scanning it
     let files = footer
         .into_directory()?
         .seek_to_files::<zip::metadata::Full>()?;
     for file in files {
-        let file = file?;
-        // print out the name of this file
-        println!("{}", String::from_utf8_lossy(&file.name));
-
-        // and subsequently look for it in the current disk
+        // resolve the files within the open archive
         // NOTE: `in_disk` could be pointed at another file for multi-file archives
-        let file = file.in_disk(reader.as_mut().cloned())?;
+        let file = file?.in_disk(reader.as_mut().cloned())?;
+        println!("{}", String::from_utf8_lossy(&file.name));
 
         // construct the decompression state and seek to the file contents
         let mut data = file
