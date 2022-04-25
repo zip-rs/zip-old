b7966a85381bdf5772ace8b9b52d5d584b094f26
Nick Babcock <nbabcock19@hotmail.com>
2022-04-25 07:00:27 -0500

    Add read fuzzing module
    As someone who has personal projects that take untrusted zips as input,
    it is important to me to be able to fuzz the zip project to simulate
    possible inputs and to ensure the projects are not vulnerable.
    
    This commit adds a cargo fuzz module for reading and extracting input.
    
    The `fuzz` directory was scaffolded with a `cargo fuzz init`
    
    I added a CI step to guard against the fuzz module decaying over time.

---
+++ fuzz/fuzz_targets/fuzz_read.rs
@@ -0,0 +1,20 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fn decompress_all(data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let reader = std::io::Cursor::new(data);
+    let mut zip = zip::ZipArchive::new(reader)?;
+
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i)?;
+        if file.size() < 1 << 20 {
+            let _ = std::io::copy(&mut file, &mut std::io::sink());
+        }
+    }
+
+    Ok(())
+}
+
+fuzz_target!(|data: &[u8]| {
+    let _ = decompress_all(data);
+});
