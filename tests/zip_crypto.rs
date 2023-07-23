ee4c560a45c5c365443b6b32504cbc346cf3dcde
Cristi Vîjdea <cristi@cvjd.me>
2023-07-23 03:07:57 +0300

    Add extra data case to zip_crypto test

--- tests/zip_crypto.rs
+++ tests/zip_crypto.rs
@@ -18,24 +18,52 @@
 // 000000c5
 
 use std::io::Cursor;
-use std::io::Read;
+use std::io::{Read, Write};
+
+use zip::result::{InvalidPassword, ZipError};
+use zip::unstable::write::FileOptionsExt;
+use zip::write::{FileOptions, ZipWriter};
+use zip::ZipArchive;
 
 #[test]
 fn encrypting_file() {
-    use zip::unstable::write::FileOptionsExt;
-    use std::io::{Read, Write};
     let mut buf = vec![0; 2048];
-    let mut archive = zip::write::ZipWriter::new(std::io::Cursor::new(&mut buf));
-    archive.start_file("name", zip::write::FileOptions::default().with_deprecated_encryption(b"password")).unwrap();
+    let mut archive = ZipWriter::new(std::io::Cursor::new(&mut buf));
+
+    archive
+        .start_file(
+            "name",
+            FileOptions::default().with_deprecated_encryption(b"password"),
+        )
+        .unwrap();
     archive.write_all(b"test").unwrap();
+
+    archive
+        .start_file_with_extra_data(
+            "name_extra",
+            FileOptions::default().with_deprecated_encryption(b"password"),
+        )
+        .unwrap();
+    archive.write_all(&[0xde, 0xad, 0x05, 0x00]).unwrap();
+    archive.write_all(b"extra").unwrap();
+    archive.end_extra_data().unwrap();
+    archive.write_all(b"test").unwrap();
+
     archive.finish().unwrap();
     drop(archive);
-    let mut archive = zip::ZipArchive::new(std::io::Cursor::new(&mut buf)).unwrap();
+
+    let mut archive = ZipArchive::new(std::io::Cursor::new(&mut buf)).unwrap();
     let mut file = archive.by_index_decrypt(0, b"password").unwrap().unwrap();
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
     assert_eq!(buf, b"test");
+    drop(file);
 
+    let mut file = archive.by_index_decrypt(1, b"password").unwrap().unwrap();
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+    assert_eq!(buf, b"test");
+    assert_eq!(&file.extra_data()[4..], b"extra");
 }
 #[test]
 fn encrypted_file() {
@@ -56,7 +84,7 @@ fn encrypted_file() {
         0x00, 0x00,
     ]);
 
-    let mut archive = zip::ZipArchive::new(zip_file_bytes).unwrap();
+    let mut archive = ZipArchive::new(zip_file_bytes).unwrap();
 
     assert_eq!(archive.len(), 1); //Only one file inside archive: `test.txt`
 
@@ -64,9 +92,7 @@ fn encrypted_file() {
         // No password
         let file = archive.by_index(0);
         match file {
-            Err(zip::result::ZipError::UnsupportedArchive(
-                zip::result::ZipError::PASSWORD_REQUIRED,
-            )) => (),
+            Err(ZipError::UnsupportedArchive(ZipError::PASSWORD_REQUIRED)) => (),
             Err(_) => panic!(
                 "Expected PasswordRequired error when opening encrypted file without password"
             ),
@@ -78,7 +104,7 @@ fn encrypted_file() {
         // Wrong password
         let file = archive.by_index_decrypt(0, b"wrong password");
         match file {
-            Ok(Err(zip::result::InvalidPassword)) => (),
+            Ok(Err(InvalidPassword)) => (),
             Err(_) => panic!(
                 "Expected InvalidPassword error when opening encrypted file with wrong password"
             ),
