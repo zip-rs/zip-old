026a49ffa7229a905a4e829b6843a583b605a880
Chris Hennick <hennickc@amazon.com>
2024-02-28 17:22:43 -0800

    Truncate huge files during read fuzz rather than skipping them

--- fuzz/fuzz_targets/fuzz_read.rs
+++ fuzz/fuzz_targets/fuzz_read.rs
@@ -1,15 +1,14 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
+use std::io::Read;
 
 fn decompress_all(data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
     let reader = std::io::Cursor::new(data);
     let mut zip = zip_next::ZipArchive::new(reader)?;
 
     for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        if file.size() <= 1 << 24 {
-            let _ = std::io::copy(&mut file, &mut std::io::sink());
-        }
+        let file = zip.by_index(i)?;
+        std::io::copy(&mut file.take(1 << 24), &mut std::io::sink())?;
     }
 
     Ok(())
