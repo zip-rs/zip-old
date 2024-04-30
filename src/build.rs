9739df01dc6f0bdabc52494bb87881afd60b276f
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-04-29 17:00:11 -0700

    build: Fix a bug where cargo syntax is different for MSRV below 1.77.0

--- src/build.rs
+++ src/build.rs
@@ -2,6 +2,6 @@ use std::env::var;
 
 fn main() {
     if var("CARGO_FEATURE_DEFLATE_MINIZ").is_ok() {
-        println!("cargo::warning=Feature `deflate-miniz` is deprecated; replace it with `deflate`");
+        println!("cargo:warning=Feature `deflate-miniz` is deprecated; replace it with `deflate`");
     }
 }
