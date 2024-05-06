b3f9170f23223d03b4469acddbcffd665d929ca2
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-05-05 18:42:49 -0700

    chore: Add `#![allow(unexpected_cfgs)]` in nightly

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@
 //!
 //!
 #![warn(missing_docs)]
+#![allow(unexpected_cfgs)] /// Needed for cfg(fuzzing) on nightly as of 2024-05-06
 
 pub use crate::compression::{CompressionMethod, SUPPORTED_COMPRESSION_METHODS};
 pub use crate::read::ZipArchive;
